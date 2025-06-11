f7bf86fee864f175df65426e5518b96198d12a7c
rin <rinbritta@gmail.com>
2025-06-11 09:20:27 +0000

    begin...?

---
+++ configuration.nix
@@ -0,0 +1,67 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./home.nix  # Home Manager stuff
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "shimeji-nix";
+  time.timeZone = "Asia/Ho_Chi_Minh";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "us";
+
+  users.users.rin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "audio" "video" ];
+    shell = pkgs.zsh;
+  };
+
+  services.xserver = {
+    enable = true;
+    windowManager.i3.enable = true;
+    displayManager.startx.enable = true;
+    xkb = {
+      layout = "us";
+    };
+  };
+
+  programs.zsh.enable = true;
+  services.redshift.enable = true;
+  services.redshift.latitude = 10.75;
+  services.redshift.longitude = 106.67;
+
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    xorg.xinit
+    kitty
+    fastfetch
+    i3status-rust
+    picom
+    rofi
+    pavucontrol
+    dunst
+    thunar
+    gtk3
+    zsh
+    btop
+    vim
+    flameshot
+    redshift
+    brightnessctl
+    ttf-jetbrains-mono-nerd
+    git
+    firefox
+  ];
+}
---
+++ home.nix
@@ -0,0 +1,51 @@
+# nixos/home.nix
+{ config, pkgs, ... }:
+
+{
+  home-manager.users.sh1m3ji = {
+    home.stateVersion = "23.11";
+
+    programs.zsh = {
+      enable = true;
+      ohMyZsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [ "git" "z" ];
+      };
+    };
+
+    home.packages = with pkgs; [
+      cbonsai
+      tty-clock
+    ];
+
+    home.file.".config/kitty" = {
+      source = ./dotfiles/kitty;
+      recursive = true;
+    };
+    home.file.".config/fastfetch" = {
+      source = ./dotfiles/fastfetch;
+      recursive = true;
+    };
+    home.file.".config/i3" = {
+      source = ./dotfiles/i3;
+      recursive = true;
+    };
+    home.file.".config/i3status-rust" = {
+      source = ./dotfiles/i3status-rust;
+      recursive = true;
+    };
+    home.file.".config/picom" = {
+      source = ./dotfiles/picom;
+      recursive = true;
+    };
+    home.file.".config/rofi" = {
+      source = ./dotfiles/rofi;
+      recursive = true;
+    };
+    home.file."Pictures/wallpapers" = {
+      source = ./dotfiles/wallpapers;
+      recursive = true;
+    };
+  };
+}
